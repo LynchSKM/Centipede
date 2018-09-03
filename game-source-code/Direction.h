55341e2749d8f3573a79fb9bebb8128728f7c93e
Lynch Mwaniki <lsking16@gmail.com>
2018-09-03 19:38:33 +0200

    Direction enum class created.

---
+++ game-source-code/Direction.h
@@ -0,0 +1,19 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+/** \enum Direction
+*   \brief is a strongly typed enum class representing the directions game
+*   objects could be facing.
+*   \author 1043475 Lynch Mwaniki and 1076467 Madimetja Sethosa.
+*
+*/
+
+enum class Direction
+{
+    UP = 0, /**< is coded as int of value 0.*/
+    DOWN,   /**< is coded as int of value 1.*/
+    LEFT,   /**< is coded as int of value 2.*/
+    RIGHT   /**< is coded as int of value 3.*/
+};
+
+#endif // DIRECTION_H
