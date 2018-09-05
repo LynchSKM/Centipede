622a3327f83ef305219f0bbc41937a8fd50b1c96
Madimetja Sethosa <sethosam@DLAB99.EIEUG>
2018-09-05 23:01:18 +0200

    Added structs to hold game object's Demensions.

---
+++ game-source-code/Dimensions.h
@@ -0,0 +1,17 @@
+#ifndef DIMENSIONS_H
+#define DIMENSIONS_H
+
+struct PlayerDimension{
+
+    const float height = 40.0;
+    const float width = 30.0;
+};
+
+struct CentipedeDemensions{
+
+    const float height = 40.0;
+    const float width = 30.0;
+
+};
+
+#endif
