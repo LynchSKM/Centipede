4157612bda664a8009cbdaa7a8c8e0e8c6b93667
Lynch Mwaniki <lsking16@gmail.com>
2018-09-09 22:19:35 +0200

    Fixed testsCentipede.cpp to use CentipedeSegment Class.

--- test-source-code/testsCentipede.cpp
+++ test-source-code/testsCentipede.cpp
@@ -1,55 +1,76 @@
-#include "Centipede.h"
+#include "CentipedeSegment.h"
+#include "Dimensions.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
 
 
-TEST_CASE("Centipede is placed at the right position."){
+TEST_CASE("Centipede Segment is placed at the right position."){
 
     Grid testGrid{650,600};
-    Centipede centipede(Direction::DOWN, Centipede::BodyType::HEAD, ObjectType::CENTIPEDE,Position{325.0f,580.0f},testGrid);
+    CentipedeSegment centipede_seg(testGrid, CentipedeSegment::BodyType::HEAD,
+                               Position{325.0f,580.0f}, Direction::DOWN);
 
-    CHECK(centipede.getPosition() == Position{325.0f,580.0f});
+    CHECK(centipede_seg.getPosition() == Position{325.0f,580.0f});
 }
 
-TEST_CASE("Centipede can be moved up, down, left and right."){
+TEST_CASE("Centipede Segment can be moved up, down, left and right."){
 
     Grid testGrid{650,600};
-    Centipede centipede(Direction::DOWN, Centipede::BodyType::HEAD, ObjectType::CENTIPEDE,Position{325.0f,300.0f},testGrid);
-    centipede.move();
+    CentipedeSegment centipede_seg(testGrid, CentipedeSegment::BodyType::HEAD,
+                               Position{325.0f,300.0f}, Direction::DOWN);
 
-    CHECK(centipede.getPosition() == Position{325.0f,305.0f});
+    struct CentipedeSegmentDemensions temp;
+    auto x = centipede_seg.getPosition().getX_pos();
+    auto y = centipede_seg.getPosition().getY_pos();
 
-    centipede.move();
-    CHECK(centipede.getPosition() == Position{320.0f,305.0f});
+    centipede_seg.move();
+    y+=temp.speed;
+    CHECK(centipede_seg.getPosition() == Position{x,y});
 
-    centipede.changeDirection();
-    centipede.move();
-    CHECK(centipede.getPosition() == Position{320.0f,310.0f});
+    centipede_seg.move();
+    x-=temp.speed;
+    CHECK(centipede_seg.getPosition() == Position{x, y});
 
-    centipede.move();
-    CHECK(centipede.getPosition() == Position{325.0f,310.0f});
+    centipede_seg.changeDirection();
+    centipede_seg.move();
+    y+=temp.speed;
+    CHECK(centipede_seg.getPosition() == Position{x, y});
+
+    centipede_seg.move();
+    x+=temp.speed;
+    CHECK(centipede_seg.getPosition() == Position{x, y});
 }
 
-TEST_CASE("Centipede move down when poisoned."){
+TEST_CASE("Centipede Segment move down when poisoned."){
 
     Grid testGrid{650,600};
-    Centipede centipede(Direction::LEFT, Centipede::BodyType::HEAD, ObjectType::CENTIPEDE,Position{325.0f,300.0f},testGrid);
+    CentipedeSegment centipede_seg(testGrid, CentipedeSegment::BodyType::HEAD,
+                               Position{325.0f,300.0f}, Direction::LEFT);
 
-    centipede.poison();
-    centipede.move();
-    centipede.move();
-    CHECK(centipede.getPosition() == Position{325.0f,310.0f});
+    struct CentipedeSegmentDemensions temp;
+    auto x = centipede_seg.getPosition().getX_pos();
+    auto y = centipede_seg.getPosition().getY_pos()+2.0f*temp.speed;
+    centipede_seg.poison();
+    centipede_seg.move();
+    centipede_seg.move();
+    CHECK(centipede_seg.getPosition() == Position{x, y});
 }
 
-TEST_CASE("Centipede turns when it hits the screen boundary."){
+
+TEST_CASE("Centipede Segment turns when it hits the screen boundary."){
 
     Grid testGrid{650,600};
-    Centipede centipede(Direction::RIGHT, Centipede::BodyType::HEAD, ObjectType::CENTIPEDE,Position{645.0f,300.0f},testGrid);
+    CentipedeSegment centipede_seg(testGrid, CentipedeSegment::BodyType::HEAD,
+                               Position{649.9f,300.0f},Direction::RIGHT);
 
-    centipede.move();
-    centipede.move();
-    CHECK(centipede.getPosition() == Position{640.0f,305.0f});
-    CHECK(centipede.getDirection()== Direction::LEFT);
+    struct CentipedeSegmentDemensions temp;
+    auto x = centipede_seg.getPosition().getX_pos()-temp.speed;
+    auto y = centipede_seg.getPosition().getY_pos()+temp.speed;
+
+    centipede_seg.move();
+    centipede_seg.move();
+    CHECK(centipede_seg.getPosition() == Position{x, y});
+    CHECK(centipede_seg.getDirection()== Direction::LEFT);
 }
 
