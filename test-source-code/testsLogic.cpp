db03ceeb1d2fbc051a98935561c8723477a3af51
Lynch Mwaniki <lsking16@gmail.com>
2018-09-09 22:22:55 +0200

    Modified testsLogic.cpp to use CentipedeSegment class.

--- test-source-code/testsLogic.cpp
+++ test-source-code/testsLogic.cpp
@@ -8,7 +8,7 @@ using std::make_shared;
 #include "Logic.h"
 #include "IMovingEntity.h"
 #include "IEntity.h"
-#include "Centipede.h"
+#include "CentipedeSegment.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest.h>
@@ -20,7 +20,7 @@ TEST_CASE("Deletion function in Logic can successfully remove dead entities")
     auto counter = 0.0f;
     while(game_objects.size() != 10){
         game_objects.push_back(
-        make_shared<Centipede>(grid,Centipede::BodyType::HEAD, Position{300,250+counter}, Direction::LEFT));
+        make_shared<CentipedeSegment>(grid,CentipedeSegment::BodyType::HEAD, Position{300,250+counter}, Direction::LEFT));
         counter +=10.0f;
     }//while
 
