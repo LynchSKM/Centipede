306efd4fbe08c32817f250696cc09fc37de0052f
Madimetja Sethosa <1076467@students.wits.ac.za>
2018-09-27 16:36:29 +0200

    Edidted game end screens.

--- game-source-code/GameOverScreen.cpp
+++ game-source-code/GameOverScreen.cpp
@@ -5,14 +5,21 @@ using namespace std::literals::string_literals;
 GameOverScreen::GameOverScreen()
 {
     //ctor
-    //gameOverTexture_.loadFromFile("Assets/GameOver.png"s);
+    gameOverTexture_.loadFromFile("Assets/GameOver.png"s);
 }
 void GameOverScreen::show(sf::RenderWindow& render_window,
             const int& player_score, const int& high_score)
 {
     //Load background texture:
     sf::Sprite background_sprite;
+    gameOverTexture_.setSmooth(true);
     background_sprite.setTexture(gameOverTexture_);
+    background_sprite.setScale(sf::Vector2f(1.2f, 1.2f));
+    sf::FloatRect backgroundRect = background_sprite.getLocalBounds();
+    background_sprite.setOrigin(backgroundRect.left + backgroundRect.width/2.0f,
+    backgroundRect.top  + backgroundRect.height/2.0f);
+    background_sprite.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,
+                                               render_window.getSize().y/2.0f));
 
     //Load font:
     font_.loadFromFile("Assets/Font.otf"s);
@@ -24,15 +31,7 @@ void GameOverScreen::show(sf::RenderWindow& render_window,
     title.setOrigin(titleRect.left + titleRect.width/2.0f,
     titleRect.top  + titleRect.height/2.0f);
     title.setFillColor(sf::Color::Green);
-    title.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f - 150));
-
-    auto game_won_controls = "Exit => Press Enter"s;
-    sf::Text controls(game_won_controls, font_);
-    sf::FloatRect textRect = controls.getLocalBounds();
-    controls.setOrigin(textRect.left + textRect.width/2.0f,
-    textRect.top  + textRect.height/2.0f);
-    controls.setFillColor(sf::Color::Red);
-    controls.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f + 150));
+    title.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f - 180));
 
     // Player score:
     auto score_text = "You scored: "+std::to_string(player_score);
@@ -42,7 +41,7 @@ void GameOverScreen::show(sf::RenderWindow& render_window,
     score.setOrigin(scoreRect.left + scoreRect.width/2.0f,
     scoreRect.top  + scoreRect.height/2.0f);
     score.setFillColor(sf::Color::Red);
-    score.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f));
+    score.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f + 230));
     // Highscore:
     auto high_score_text = "Highscore: "s+std::to_string(high_score);
     sf::Text game_high_score(high_score_text, font_);
@@ -50,21 +49,41 @@ void GameOverScreen::show(sf::RenderWindow& render_window,
     game_high_score.setOrigin(highRect.left + highRect.width/2.0f,
     highRect.top  + highRect.height/2.0f);
     game_high_score.setFillColor(sf::Color::Red);
-    game_high_score.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f +30));
+    game_high_score.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f +260));
 
-    //Draw:
+    auto game_Over_controls = "Exit => Press Enter"s;
+    sf::Text controls(game_Over_controls, font_);
+    sf::FloatRect textRect = controls.getLocalBounds();
+    controls.setOrigin(textRect.left + textRect.width/2.0f,
+    textRect.top  + textRect.height/2.0f);
+    controls.setFillColor(sf::Color::Red);
+    controls.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f + 300));
 
-	render_window.clear(sf::Color(0,0,0));
-    //render_window.draw(background_sprite);
-    render_window.draw(title);
-    render_window.draw(controls);
-    render_window.draw(score);
-    render_window.draw(game_high_score);
-    render_window.display();
-
-    // Poll for keyboard event:
+    // Poll for keyboard event and Draw:
+    auto changecolour = true;
+    auto counter = 0;
     sf::Event event;
-    while(true){
+    while(true)
+    {
+        render_window.clear(sf::Color(0,0,0));
+        render_window.draw(background_sprite);
+        if(changecolour && counter%5 == 0)
+        {
+            controls.setFillColor(sf::Color::Green);
+            title.setFillColor(sf::Color::Red);
+            changecolour = !changecolour;
+        }else{
+            controls.setFillColor(sf::Color::Red);
+            title.setFillColor(sf::Color::Green);
+            changecolour = !changecolour;
+        }
+        ++counter;
+        render_window.draw(title);
+        render_window.draw(controls);
+        render_window.draw(score);
+        render_window.draw(game_high_score);
+        render_window.display();
+
         while(render_window.pollEvent(event)){
             if(event.key.code == sf::Keyboard::Enter || event.type == sf::Event::Closed){
                 render_window.close();
--- game-source-code/GameWonScreen.cpp
+++ game-source-code/GameWonScreen.cpp
@@ -4,15 +4,25 @@ using namespace std::literals::string_literals;
 GameWonScreen::GameWonScreen()
 {
     //ctor
-    //gameWonTexture_.loadFromFile("Assets/Winner Background.png"s);
+    gameWonTexture_.loadFromFile("Assets/GameWon.png"s);
 }
 
 void GameWonScreen::show(sf::RenderWindow& render_window,
           const int& player_score, const int& high_score)
 {
     //Load background texture:
-    //sf::Sprite background_sprite;
-    //background_sprite.setTexture(gameWonTexture_);
+    sf::Sprite background_sprite;
+    gameWonTexture_.setSmooth(true);
+    gameWonTexture_.setSmooth(true);
+    gameWonTexture_.setSmooth(true);
+    gameWonTexture_.setSmooth(true);
+    background_sprite.setTexture(gameWonTexture_);
+    background_sprite.setScale(sf::Vector2f(1.5f, 1.5f));
+    sf::FloatRect backgroundRect = background_sprite.getLocalBounds();
+    background_sprite.setOrigin(backgroundRect.left + backgroundRect.width/2.0f,
+    backgroundRect.top  + backgroundRect.height/2.0f);
+    background_sprite.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,
+                                               render_window.getSize().y/2.0f));
 
     //Load font:
     font_.loadFromFile("Assets/Font.otf"s);
@@ -25,15 +35,9 @@ void GameWonScreen::show(sf::RenderWindow& render_window,
     titleRect.top  + titleRect.height/2.0f);
     //title.setStyle(sf::Text::Bold);
     title.setFillColor(sf::Color::Green);
-    title.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f - 150));
+    title.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,
+                                   render_window.getSize().y/2.0f - 260));
 
-    auto game_won_controls = "Exit -> Press Enter"s;
-    sf::Text controls(game_won_controls, font_);
-    sf::FloatRect textRect = controls.getLocalBounds();
-    controls.setOrigin(textRect.left + textRect.width/2.0f,
-    textRect.top  + textRect.height/2.0f);
-    controls.setFillColor(sf::Color::Red);
-    controls.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f + 150));
 
     // Player score:
     auto score_text = "\t You scored: "s+std::to_string(player_score);
@@ -42,8 +46,9 @@ void GameWonScreen::show(sf::RenderWindow& render_window,
     sf::FloatRect scoreRect = score.getLocalBounds();
     score.setOrigin(scoreRect.left + scoreRect.width/2.0f,
     scoreRect.top  + scoreRect.height/2.0f);
-    score.setFillColor(sf::Color::Red);
-    score.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f));
+    score.setFillColor(sf::Color(255, 165, 0));
+    score.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,
+                                   render_window.getSize().y/2.0f+ 230));
 
     // Highscore:
     auto high_score_text = "\t Highscore: "s+std::to_string(high_score);
@@ -51,22 +56,48 @@ void GameWonScreen::show(sf::RenderWindow& render_window,
     sf::FloatRect highRect = game_high_score.getLocalBounds();
     game_high_score.setOrigin(highRect.left + highRect.width/2.0f,
     highRect.top  + highRect.height/2.0f);
-    game_high_score.setFillColor(sf::Color::Red);
-    game_high_score.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,render_window.getSize().y/2.0f +30));
-    //game_high_score.setStyle(sf::Text::Bold);
+    game_high_score.setFillColor(sf::Color(255, 165, 0));
+    game_high_score.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,
+                                             render_window.getSize().y/2.0f +260));
+
+
+    auto game_won_controls = "Exit -> Press Enter"s;
+    sf::Text controls(game_won_controls, font_);
+    sf::FloatRect textRect = controls.getLocalBounds();
+    controls.setOrigin(textRect.left + textRect.width/2.0f,
+                        textRect.top  + textRect.height/2.0f);
+    controls.setFillColor(sf::Color::Red);
+    controls.setPosition(sf::Vector2f(render_window.getSize().x/2.0f,
+                                      render_window.getSize().y/2.0f + 300));
+
 
     //Draw:
-    //render_window.draw(background_sprite);
-	render_window.clear(sf::Color(0,0,0));
-    render_window.draw(title);
-    render_window.draw(controls);
-    render_window.draw(score);
-    render_window.draw(game_high_score);
-    render_window.display();
 
     // Poll for keyboard event:
+    auto changecolour = true;
+    auto counter = 0;
     sf::Event event;
-    while(true){
+    while(true)
+    {
+        render_window.clear(sf::Color(0,0,0));
+        render_window.draw(background_sprite);
+        if(changecolour && counter%4 == 0)
+        {
+            controls.setFillColor(sf::Color::Green);
+            title.setFillColor(sf::Color::Red);
+            changecolour = !changecolour;
+        }else{
+            controls.setFillColor(sf::Color::Red);
+            title.setFillColor(sf::Color::Green);
+            changecolour = !changecolour;
+        }
+        ++counter;
+        render_window.draw(title);
+        render_window.draw(controls);
+        render_window.draw(score);
+        render_window.draw(game_high_score);
+        render_window.display();
+
         while(render_window.pollEvent(event)){
             if(event.key.code == sf::Keyboard::Enter || event.type == sf::Event::Closed){
                 render_window.close();
